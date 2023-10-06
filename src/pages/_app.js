c93f3891989a6c8bbc1be6ae81943e7da00b17b1
mohammad nishad <mohammadnishad1243.mn@gmail.com>
2023-10-06 16:33:26 +0530

    done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+  const getLayout = getLayout.Component || ((page) => page)
   return <Component {...pageProps} />
 }
